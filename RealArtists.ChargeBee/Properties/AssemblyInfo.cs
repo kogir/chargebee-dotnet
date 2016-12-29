043313a95c4a7fddc108eacc9d3bb7fa586a9f53
Nick Sivo <nick@kogir.com>
2016-12-28 22:31:24 -0700

    Bump version.

--- RealArtists.ChargeBee/Properties/AssemblyInfo.cs
+++ RealArtists.ChargeBee/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
 [assembly: AssemblyCompanyAttribute("kogir")]
 [assembly: AssemblyProductAttribute("RealArtists.ChargeBee")]
 [assembly: AssemblyCopyrightAttribute("©ChargeBee Inc. and Nick Sivo. See LICENSE.")]
